7a4ae921cde246c9b4eef0b6ba02e943760feae0
Michael <Michael@ftwnet.co.uk>
2013-11-19 01:17:27 +0000

    Added all the jazz!

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ Automatic-dnsmasq-update
 ========================
 
 Automatic-dnsmasq-update
+
+
+This script will auto update your list for pgl.yoyo.org.
+
+========================
+Make sure you are running as root & have curl installed.
+
+========================
+
+This will add the list to your dnsmasq config so it can start hijacking the address.
+```bash
+echo "conf-file=/etc/ads.dnsmasq.conf" >> /etc/dnsmasq.conf
+```
+
+```bash
+curl -s URL HERE | bash
+```
+========================
---
+++ dnsmasq.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo -e "\E[33;40m[*] Checking if dnsmasq is installed.\e[0m"
+if [ -f /etc/dnsmasq.conf ];  then
+  echo -e "\E[32;40m dnsmasq is installed!!! \e[0m"
+  echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
+  sleep 2
+  curl -O /etc/ads.dnsmasq.conf  "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext"
+  sleep 2
+  echo -e "reloading dnsmasq"
+  /etc/init.d/dnsmasq restart
+  echo -e "\E[32;40m Done! \e[0m"
+  exit 1
+else 
+  echo -e "\e[0;31m dnsmasq is not installed or missing \e[0m"
+  exit 1
+fi
+
