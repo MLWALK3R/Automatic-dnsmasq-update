f6e85cc98baceb114f3ba768165e2b72f9d0de77
nathanthorpe <nathan.thorpe@comcast.net>
2013-11-18 18:00:35 -0800

    Fix colors

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -3,10 +3,10 @@ echo -e "\E[33;40m[*] Checking if dnsmasq is installed.\e[0m"
 if [ -f /etc/dnsmasq.conf ];  then
 	echo -e "\E[32;40m dnsmasq is installed!!! \e[0m"
 	sleep 1
-	echo -e "\E[32;40m Checking the dnsmasq config \e[0m"
+	echo -e "\E[33;40m Checking the dnsmasq config \e[0m"
 	TEST=`grep ads.dnsmasq.conf /etc/dnsmasq.conf`
 	if [ "$TEST" != "conf-file=/etc/ads.dnsmasq.conf" ]; then
-		echo -e "\E[32;40m[*] This is your first time running this script! [*]\e[0m"
+		echo -e "\E[32;40mThis is your first time running this script!\e[0m"
 		echo "conf-file=/etc/ads.dnsmasq.conf" >> /etc/dnsmasq.conf
 		sleep 1
 		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
