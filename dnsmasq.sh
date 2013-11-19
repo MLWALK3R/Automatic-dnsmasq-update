01c6f33bce287e890bed278b23060834320fe523
nathanthorpe <nathan.thorpe@comcast.net>
2013-11-18 17:55:59 -0800

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -6,6 +6,9 @@ if [ -f /etc/dnsmasq.conf ];  then
 	echo -e "\E[32;40m Checking the dnsmasq config \e[0m"
 	TEST=`grep ads.dnsmasq.conf /etc/dnsmasq.conf`
 	if [ "$TEST" != "conf-file=/etc/ads.dnsmasq.conf" ]; then
+		echo -e "\E[32;40m[*] This is your first time running this script! [*]\e[0m"
+		echo "conf-file=/etc/ads.dnsmasq.conf" >> /etc/dnsmasq.conf
+		sleep 1
 		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
 		sleep 2
 		curl -o  /etc/ads.dnsmasq.conf  "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext"
@@ -14,9 +17,6 @@ if [ -f /etc/dnsmasq.conf ];  then
 		echo -e "\E[32;40m Done! \e[0m"
 		exit 1
 	else 
-		echo -e "\E[32;40m[*] This is your first time running this script! [*]\e[0m"
-		echo "conf-file=/etc/ads.dnsmasq.conf" >> /etc/dnsmasq.conf
-		sleep 1
 		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
 		sleep 2
 		curl -o  /etc/ads.dnsmasq.conf  "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext"
