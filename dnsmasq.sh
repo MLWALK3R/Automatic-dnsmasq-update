4be0d39d8485a030c3eb280b720d0f963bebd2d1
Michael <Michael@ftwnet.co.uk>
2013-11-20 12:29:11 +0000

    Fixed some issue change the install method to support more devices.

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -1,48 +1,26 @@
 #!/bin/sh
-echo -e "\E[33;40m[*] Checking if you're running an Edgemax device.\e[0m"
-if [ -f /config/config.boot ]; then
-	echo -e "\E[32;40m Edge router detected \e[0m"
-	echo -e "\E[33;40m[*] Checking if dnsmasq is installed.\e[0m"
-	TEST=` find /etc/dnsmasq.d `
-	if [ "$TEST" != "/etc/dnsmasq.d" ]; then
-		echo -e "Installed/found"
-	        sleep 1
-		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
-		sleep 2
-		curl -s "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext" > /etc/ads.dnsmasq.conf
-		echo -e "reloading dnsmasq"
-		/etc/init.d/dnsmasq restart
-		echo -e "\E[32;40m Done! \e[0m"
-		exit 1
-	else
-		echo -e "\e[0;31m dnsmasq is not installed or missing \e[0m"
-		exit 1
-		fi
-else
-	echo -e "\E[33;40m Edgemax not found \e[0m"
 	echo -e "\E[33;40m[*] Checking if dnsmasq is installed.\e[0m"
 	if [ -f /etc/dnsmasq.conf ]; then
 	echo -e "\E[32;40m dnsmasq is installed \e[0m"
 	sleep 1
-	echo -e "\E[33;40m[*] Checking the dnsmasq config [*]\e[0m"
-	TEST=`grep ads.dnsmasq.conf /etc/dnsmasq.conf`
-	if [ "$TEST" != "conf-file=/etc/ads.dnsmasq.conf" ]; then
-		echo -e "\E[32;40m[*] This is your first time running this script! [*]\e[0m"
-		echo "conf-file=/etc/ads.dnsmasq.conf" >> /etc/dnsmasq.conf
-		sleep 1
+	echo -e "\E[33;40m[*] Checking the dnsmasq.d folder [*]\e[0m"
+    if [ -f /etc/dnsmasq.d/ads.dnsmasq.conf  ];  then 
 		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
 		sleep 2
-		curl -s "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext" > /etc/ads.dnsmasq.conf
-		echo -e "reloading dnsmasq"
-		/etc/init.d/dnsmasq restart
-		echo -e "\E[32;40m Done! \e[0m"
-	else 
-		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
-		sleep 2
-		curl -s "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext" > /etc/ads.dnsmasq.conf
+		curl -s "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext" > /etc/dnsmasq.d/ads.dnsmasq.conf
 		echo -e "reloading dnsmasq"
 		/etc/init.d/dnsmasq restart
 		echo -e "\E[32;40m updated ad list \e[0m"
+	else 
+		echo -e "\E[32;40m[*] This is your first time running this script! [*]\e[0m"
+		sleep 1
+		echo -e "\E[32;40m[*] Updating your ad-block list [*]\e[0m"
+		sleep 2
+		curl -s "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro0&mimetype=plaintext" > /etc/dnsmasq.d/ads.dnsmasq.conf
+		echo -e "reloading dnsmasq"
+		/etc/init.d/dnsmasq restart
+		echo -e "\E[32;40m Done! \e[0m"
+		
 		fi
 else 
 	echo -e "\e[0;31m dnsmasq is not installed or missing \e[0m"
@@ -63,5 +41,4 @@ else
 		echo -e "\E[32;40m Ok have it your way \e[0m"
 		exit 1
   fi
-fi
 fi
